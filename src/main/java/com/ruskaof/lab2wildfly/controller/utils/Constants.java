0e771d0b853e9de80886fe7a84ea4708965e954a
Dmitrii Rusinov <199-41@mail.ru>
2022-10-08 15:29:12 +0300

    Move client id, client secret key to environment variables

--- src/main/java/com/ruskaof/lab2wildfly/controller/utils/Constants.java
+++ src/main/java/com/ruskaof/lab2wildfly/controller/utils/Constants.java
@@ -1,8 +1,18 @@
 package com.ruskaof.lab2wildfly.controller.utils;
 
+import java.util.Map;
+
 public final class Constants {
-    public static final int CLIENT_ID = 51433610;
-    public static final String CLIENT_SECRET = "ka2ybPtS0YoEU6WBMEFX";
+    public static final String CLIENT_ID;
+    public static final String CLIENT_SECRET;
+
+    static {
+        final Map<String, String> env = System.getenv();
+        CLIENT_ID = env.get("LAB2_VK_CLIENT_ID");
+        CLIENT_SECRET = env.get("LAB2_VK_CLIENT_SECRET");
+
+        System.out.println(CLIENT_ID + " " + CLIENT_SECRET);
+    }
 
     private Constants() {
         throw new UnsupportedOperationException("This is a util class that must not be initialised");
